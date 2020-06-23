64d28be192095723c147cc5cde8f4f3b0733cce5
Rahat Sarawasee <rahat.sar@ascendcorp.com>
2020-06-23 23:49:38 +0700

    first commit

---
+++ src/main/java/com/tangbear/springbootcontract/SpringBootContractApplication.java
@@ -0,0 +1,13 @@
+package com.tangbear.springbootcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootContractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tangbear/springbootcontract/SpringBootContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangbear.springbootcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
